3093fb000429f2ce057d79ae9d0f9c8e3b17ffbb
ShirChoi <abdulloevsarvar2005@gmail.com>
2021-08-05 21:20:40 +0500

    some fixes

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+namespace Problem1 {
+    class Program {
+        static void Main(string[] args) {
+            double a = Read("a");
+            double b = Read("b");
+
+            System.Console.WriteLine($"Result = {Math.Sqrt(a * b)}");
+        }
+
+        static double Read(string varName) {
+            Console.Write(varName + " = ");
+            return double.Parse(Console.ReadLine());
+        } 
+    }
+}
